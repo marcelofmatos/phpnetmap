bec1a88a12de54698f07de6fc482a7a355dc4fd4
Marcelo Matos <contato@marcelomatos.dev>
2026-07-24 17:19:12 -0400

    Adicionar partials de estado vazio das abas Documentação e Histórico

---
+++ protected/views/host/_tabDocs.php
@@ -0,0 +1,8 @@
+<?php
+/* @var $this HostController */
+/* @var $model Host */
+?>
+<div class="tab-empty-state">
+    <p>Nenhuma nota registrada ainda.</p>
+    <p class="tab-empty-hint">(recurso chega numa próxima etapa)</p>
+</div>
---
+++ protected/views/host/_tabHistory.php
@@ -0,0 +1,8 @@
+<?php
+/* @var $this HostController */
+/* @var $model Host */
+?>
+<div class="tab-empty-state">
+    <p>Nenhum registro de manutenção ainda.</p>
+    <p class="tab-empty-hint">(recurso chega numa próxima etapa)</p>
+</div>
