32183f4d0b0ad94c2207f1771fbeb5f4dca2e02d
Marcelo Matos <contato@marcelomatos.dev>
2026-07-24 14:12:46 -0400

    Remover verificação da existência do arquivo .htpasswd antes de executar set_htpasswd.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -4,6 +4,7 @@ set -e
 # Sobe o daemon SNMP local e gera o htpasswd antes de entregar ao Apache.
 service snmpd start || true
 
-[ -f /app/.htpasswd ] || /app/set_htpasswd.sh
+#[ -f /app/.htpasswd ] || /app/set_htpasswd.sh
+/app/set_htpasswd.sh
 
 exec "$@"
