9ea6b896c71697f3c59b75248f8556370a0536a5
Marcelo Matos <contato@marcelomatos.dev>
2026-07-24 13:53:33 -0400

    Adicionar verificação para a existência do arquivo .htpasswd antes de gerar com set_htpasswd.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,6 +3,7 @@ set -e
 
 # Sobe o daemon SNMP local e gera o htpasswd antes de entregar ao Apache.
 service snmpd start || true
-/app/set_htpasswd.sh
+
+[ -f /app/.htpasswd ] || /app/set_htpasswd.sh
 
 exec "$@"
